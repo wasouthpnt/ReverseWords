62d418cd6f33658647b3ba1c9f49580d3041533c
Stick <wasouthpnt@aol.com>
2019-06-28 23:17:37 -0400

    added main method and solution files

--- ReverseWordsInSentence.cs
+++ ReverseWordsInSentence.cs
@@ -1,8 +1,41 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ReverseWordsInSentence
 {
+    /// <summary>
+    /// Main
+    /// </summary>
+    /// <param name="args">Ignored</param>
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Input your sentence:");
+            string sInputString = Console.ReadLine();
+            string[] words = sInputString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // assume at least one space between two single letter words minimum.
+            if (words.Length >= 2)
+            {
+                String sResult = ReverseString(sInputString);
+                if (sResult == String.Empty)
+                    Console.WriteLine("Error Entry");
+                else
+                    Console.WriteLine(sResult);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
 
     /// <summary>
     /// Reverse words in a string and return it, or return and empty string on failure.
