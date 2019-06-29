7071665c93428e3d2910f81b86f8a00106ba66a4
Stick <wasouthpnt@aol.com>
2019-06-29 08:07:54 -0400

    Added TestReverseMultiWord

---
+++ UnitTestReverseWords/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestReverseWords
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestReverseMultiWord()
+        {
+            string Expected = "string test my is This";
+            string result = ReverseWordsInSentence.ReverseString("This is my test string");
+            Assert.AreEqual(Expected, result);
+        }
+    }
+}
