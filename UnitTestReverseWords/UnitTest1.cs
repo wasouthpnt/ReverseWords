43cbce1c607f5d73848ec9728e42c45ffcc16882
Stick <wasouthpnt@aol.com>
2019-06-29 08:24:54 -0400

    added several more unit tests

--- UnitTestReverseWords/UnitTest1.cs
+++ UnitTestReverseWords/UnitTest1.cs
@@ -13,5 +13,30 @@ namespace UnitTestReverseWords
             string result = ReverseWordsInSentence.ReverseString("This is my test string");
             Assert.AreEqual(Expected, result);
         }
+
+        
+        [TestMethod]
+        public void TestReverseSingleChars()
+        {
+            string Expected = "b a";
+            string result = ReverseWordsInSentence.ReverseString("a b");
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void TestReverseOneChar()
+        {
+            string Expected = " a";
+            string result = ReverseWordsInSentence.ReverseString("a ");
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void TestReverseSingleWord()
+        {
+            string Expected = "Hello";
+            string result = ReverseWordsInSentence.ReverseString("Hello");
+            Assert.AreEqual(Expected, result);
+        }
     }
 }
